f9cdf668b3860dd0bf935cfe03d9433e3579dabc
chrisamico <eyeseast@gmail.com>
2015-03-01 11:27:59 -0500

    Load more aggregates into redis, with database and flush options

--- bin/load_redis.py
+++ bin/load_redis.py
@@ -4,21 +4,32 @@ Load names from sqlite database into redis
 """
 from __future__ import unicode_literals
 
+import argparse
+import itertools
 import sys
 import dataset
 from redis import StrictRedis
 
-DATABASE_URL = "sqlite:///names.db"
+DEFAULT_DATABASE_URL = "sqlite:///names.db"
 DEFAULT_REDIS_URL = "redis://localhost:6379/1"
 TABLE_NAME = "names"
 
-def main(db_url, redis_url):
+RANKS = [5, 50, 500, 1000]
+
+def main(db_url, redis_url, flush=False):
     """
     Fetch from DB. Load to redis.
     """
     db = dataset.connect(db_url)
     table = db[TABLE_NAME]
     redis = StrictRedis.from_url(redis_url)
+
+    # flush or not
+    if flush:
+        print('Flushing database at {}'.format(redis_url))
+        redis.flushdb()
+
+    # pipeline lots of things
     pipe = redis.pipeline()
 
     # all names
@@ -39,11 +50,41 @@ def main(db_url, redis_url):
 
     pipe.execute()
 
+    # popularity
+    for sex, rank in itertools.product(['F', 'M'], RANKS):
+        sql = "SELECT distinct(name) FROM {} WHERE sex = :sex AND rank <= :rank".format(TABLE_NAME)
+        print('Loading: {sex}, {rank}'.format(sex=sex, rank=rank))
+
+        for row in db.query(sql, sex=sex, rank=rank):
+            key = ':'.join(['names', sex.lower(), str(rank)])
+            pipe.sadd(key, row['name'])
+
+        # execute for each combination
+        pipe.execute()
+
+    # store the unions
+    for rank in RANKS:
+        pipe.sunionstore(
+            'names:all:{}'.format(rank),
+            'names:f:{}'.format(rank),
+            'names:m:{}'.format(rank)
+        )
+
+    pipe.execute()
+
+# arg parsing
+parser = argparse.ArgumentParser(
+    description='Load aggregates from a database into redis')
+
+parser.add_argument('database_uri', default=DEFAULT_DATABASE_URL,
+    help='Connection URI for source database')
+
+parser.add_argument('redis_uri', default=DEFAULT_REDIS_URL,
+    help='Connection URI for redis client')
+
+parser.add_argument('--flush', default=False, action='store_true')
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        redis_url = sys.argv[1]
-    else:
-        redis_url = DEFAULT_REDIS_URL
 
-    main(DATABASE_URL, redis_url)
+    args = parser.parse_args()
+    main(args.database_uri, args.redis_uri, args.flush)
