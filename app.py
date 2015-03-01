a6aae2d94f325d0d39d89045f70d12dcbbd21480
chrisamico <eyeseast@gmail.com>
2015-02-28 22:48:07 -0500

    Maybe a database is the simplest way to do this

--- app.py
+++ app.py
@@ -7,14 +7,28 @@ from flask import Flask, g, render_template, jsonify
 from redis import StrictRedis
 
 # using this for prototyping
-DATABASE_URL = "sqlite:///names.db"
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///names.db')
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/1')
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-redis = StrictRedis.from_url(REDIS_URL)
-db = dataset.connect(DATABASE_URL)
+
+def get_db():
+    "Connect to DB"
+    db = g.get('db', None)
+    if db is None:
+        db = dataset.connect(DATABASE_URL)
+    return db
+
+
+def get_redis():
+    "Get a redis connection"
+    redis = g.get('redis', None)
+    if redis is None:
+        redis = StrictRedis.from_url(REDIS_URL)
+    return redis
+
 
 @app.route('/')
 def index():
@@ -26,12 +40,22 @@ def index():
 @app.route('/name/<group>')
 def get_random_name(group=None):
     "Get a random name"
+    redis = get_redis()
     if group in ('f', 'm', 'all'):
         key = "names:{}".format(group)
     else:
         key = "names:all"
 
     return redis.srandmember(key)
+
+
+@app.route('/names')
+@app.route('/names/<group>')
+def get_random_stats(group=None):
+    "Get a random name, with stats"
+    name = get_random_name(group=group)
+    stats = get_name_stats(name, sex=group)
+    return jsonify(name=name, stats=stats)
 
 
 @app.route('/<name>')
@@ -44,6 +68,7 @@ def name_stats(name, sex=None):
 
 def get_name_stats(name, sex=None):
     "Get stats for a single name"
+    db = get_db()
     table = db['names']
     where = {'name': name}
     if sex:
